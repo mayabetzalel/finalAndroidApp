6fc5fe258fa54abbdbadb9ea778b03b1b38f0527
lotan yuran <lotan759@gmail.com>
2023-03-04 16:28:23 +0200

    add register page ui and fix gitignore

--- app/src/main/java/com/example/class3demo2/LoginActivity.java
+++ app/src/main/java/com/example/class3demo2/LoginActivity.java
@@ -10,13 +10,14 @@ import android.widget.TextView;
 public class LoginActivity extends AppCompatActivity {
 
     EditText emailET, passwordET;
+//    FirebaseAuth mAuth = FirebaseAuth.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        TextView goToRegister = findViewById(R.id.login_tv_register_page);
+        TextView goToRegister = findViewById(R.id.register_tv_login_page);
 
         goToRegister.setOnClickListener(view -> {
             Intent intent = new Intent(this, RegisterActivity.class);
@@ -26,5 +27,41 @@ public class LoginActivity extends AppCompatActivity {
         emailET = findViewById(R.id.login_email_et);
         passwordET = findViewById(R.id.login_password_et);
 
+//        Button loginButton = findViewById(R.id.login_btn_login);
+//
+//        loginButton.setOnClickListener(view -> {
+//
+//            String email = emailET.getText().toString();
+//            String password = passwordET.getText().toString();
+//
+//            if (TextUtils.isEmpty(email)) {
+//                Toast.makeText(LoginActivity.this, "Enter email",
+//                        Toast.LENGTH_SHORT).show();
+//                return;
+//            }
+//
+//            if (TextUtils.isEmpty(password)) {
+//                Toast.makeText(LoginActivity.this, "Enter password",
+//                        Toast.LENGTH_SHORT).show();
+//                return;
+//            }
+//
+//            mAuth.signInWithEmailAndPassword(email, password)
+//                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+//                        @Override
+//                        public void onComplete(@NonNull Task<AuthResult> task) {
+//                            if (task.isSuccessful()) {
+//                                // Go to main app
+//                                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+//                                startActivity(intent);
+//                                finish();
+//                            } else {
+//                                Toast.makeText(LoginActivity.this, "Authentication failed.",
+//                                        Toast.LENGTH_SHORT).show();
+//                            }
+//                        }
+//                    });
+//
+//        });
     }
 }
--- app/src/main/java/com/example/class3demo2/RegisterActivity.java
+++ app/src/main/java/com/example/class3demo2/RegisterActivity.java
@@ -2,7 +2,9 @@ package com.example.class3demo2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        TextView goToLogin = findViewById(R.id.register_tv_login_page);
+
+        goToLogin.setOnClickListener(view -> {
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+        });
     }
 }
