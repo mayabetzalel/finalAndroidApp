e7b61739403eaff6a786839d41bd03aad2e0b653
Maya Betzalel <mayab882@gmail.com>
2023-03-09 12:29:19 +0200

    Adjust information data fragment

--- app/src/main/java/com/example/class3demo2/BlueFragment.java
+++ app/src/main/java/com/example/class3demo2/BlueFragment.java
@@ -1,11 +1,15 @@
 package com.example.class3demo2;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -13,9 +17,15 @@ import androidx.navigation.Navigation;
 
 public class BlueFragment extends Fragment {
     TextView titleTv;
+    ImageView avatarImage;
+    TextView idTv;
     String title;
 
-    public static BlueFragment newInstance(String title){
+    String id;
+
+    String img;
+
+    public static BlueFragment newInstance(String title, String id, String img ){
         BlueFragment frag = new BlueFragment();
         Bundle bundle = new Bundle();
         bundle.putString("TITLE",title);
@@ -29,6 +39,7 @@ public class BlueFragment extends Fragment {
         Bundle bundle = getArguments();
         if (bundle != null){
             this.title = bundle.getString("TITLE");
+            this.id = bundle.getString("ID");
         }
     }
 
@@ -39,10 +50,20 @@ public class BlueFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_blue, container, false);
 
         title = BlueFragmentArgs.fromBundle(getArguments()).getBlueTitle();
+        id = BlueFragmentArgs.fromBundle(getArguments()).getBlueId();
+        img = BlueFragmentArgs.fromBundle(getArguments()).getBlueImage();
 
         TextView titleTv = view.findViewById(R.id.bluefrag_title_tv);
-        if (title != null){
+        TextView idTv = view.findViewById(R.id.bluefrag_id_tv);
+        ImageView imgTv = view.findViewById(R.id.bluefrag_image_tv);
+        if (title != null) {
             titleTv.setText(title);
+        }
+        if (id != null) {
+            idTv.setText(id);
+        }
+        if(img != null) {
+            Picasso.get().load(img).into(imgTv);
         }
 
         View button = view.findViewById(R.id.bluefrag_back_btn);
